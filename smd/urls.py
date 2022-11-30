27d1429b75859795ef61073c784397fd250c0488
Andrés Bustos <afbustosv@gmail.com>
2022-11-29 21:40:17 -0500

    login y logout

--- smd/urls.py
+++ smd/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    path('members/', views.members, name='members')
+    path('members/', views.members, name='members'),
+    path('logout/', views.signout, name='signout'),
+    path('signin/', views.signin, name='signin')
 ]
